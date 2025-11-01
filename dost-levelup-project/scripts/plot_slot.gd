6214a2b9a6678ebc8d284eae3376d4f0e62eca9a
Franaffel <franksbass19@gmail.com>
2025-11-01 19:44:57 +0800

    sjjask

--- dost-levelup-project/scripts/plot_slot.gd
+++ dost-levelup-project/scripts/plot_slot.gd
@@ -4,7 +4,7 @@ var plot_index = [0, 0]
 var current_building = null
 var is_occupied = false
 var building_scene = null
-var adjacent_plot_indices = [] #all adjacent plots, max of 8
+var adjacent_plot_indices = [] # all adjacent plots, max of 8
 var board_owner = ""
 
 func check_occupied():
@@ -12,7 +12,6 @@ func check_occupied():
 
 func set_plot_index(index: Array) -> void:
 	plot_index = index
-	# Calculate adjacent plots
 	adjacent_plot_indices.clear()
 	for x_offset in range(-1, 2):
 		for y_offset in range(-1, 2):
@@ -24,20 +23,13 @@ func set_plot_index(index: Array) -> void:
 				continue
 			var adjacent_index = [plot_index[0] + x_offset, plot_index[1] + y_offset]
 			adjacent_plot_indices.append(adjacent_index)
-	print("adj plots")
-	print(adjacent_plot_indices)
 
 func trigger_disaster(card_id: int, disaster_instance):
-	print("disaster go breeeeeeeeeeeeeee")
 	if is_occupied and building_scene:
 		match card_id:
-			9: #black_out
-				print("blackout")
+			9: #blackout
 				building_scene.blackout()
-				print("Building at ", plot_index, " disabled") 
-
-			10: #earthquake 3x3 aree
-				print("urttth")
+			10: #area 3x3 quakey
 				await get_tree().create_timer(0.8).timeout
 				var parent_node = get_parent().get_parent()
 				for adj_index in adjacent_plot_indices:
@@ -45,9 +37,7 @@ func trigger_disaster(card_id: int, disaster_instance):
 					if tile and tile.is_occupied and tile.building_scene:
 						tile.building_scene.take_damage(30, "quakes")
 				building_scene.take_damage(30, "quakes")
-
-			11: #strike 3x3 aree
-				print("fire")
+			11: #area 3x3 meterorrer
 				await get_tree().create_timer(0.8).timeout
 				var parent_node = get_parent().get_parent()
 				for adj_index in adjacent_plot_indices:
@@ -55,55 +45,47 @@ func trigger_disaster(card_id: int, disaster_instance):
 					if tile and tile.is_occupied and tile.building_scene:
 						tile.building_scene.take_damage(30, "fire")
 				building_scene.take_damage(30, "fire")
-
-			12: # tornado
-
+			12: #townaddooooooo
 				await get_tree().create_timer(0.8).timeout
-
 				if building_scene:
 					building_scene.take_damage(5, "wind")
-					print("Tornado damaged building at ", plot_index)
 
 				await get_tree().create_timer(0.8).timeout
 
-				var roll: int
 				if multiplayer.is_server():
-					roll = randi_range(1, 100)
+					var roll = randi_range(1, 100)
+					var new_index = []
+					if roll <= 35 and adjacent_plot_indices.size() > 0:
+						new_index = adjacent_plot_indices.pick_random()
 					if board_owner == "player":
-						# The tornado occured towards host, so other player shows it at opponent board
 						var opponent_tile = get_tree().root.get_node("Game/OpponentPlot").get_tile_at(plot_index)
-						opponent_tile.rpc("sync_tornado_roll", roll)
+						opponent_tile.rpc("sync_tornado_roll", roll, new_index)
 					else:
-						# The tornado occured towards client, so host shows it at player board
 						var player_tile = get_tree().root.get_node("Game/PlayerPlot").get_tile_at(plot_index)
-						player_tile.rpc("sync_tornado_roll", roll)
-					# Host also processes the roll	
-					sync_tornado_roll(roll)
+						player_tile.rpc("sync_tornado_roll", roll, new_index)
+					sync_tornado_roll(roll, new_index)
 				else:
-					return # clients will wait for host to send roll
+					return
 
 
 @rpc("reliable")
-func sync_tornado_roll(roll: int):
-	
+func sync_tornado_roll(roll: int, new_index: Array):
 	var card_res = ResourceLoader.load("res://cards/card_12.tres")
 	var disaster_scene = card_res.disaster_scene
 
-	if roll <= 30:
-		if adjacent_plot_indices.size() > 0:
+	if roll <= 35: #35% chance to move it 
+		if new_index.size() == 2:
 			var parent_node = get_parent().get_parent()
-			var new_index = adjacent_plot_indices.pick_random()
-			print("Tornado spreads to ", new_index)
 			var target_tile = parent_node.get_tile_at(new_index)
 			if target_tile:
 				var disaster_instance = disaster_scene.instantiate()
 				target_tile.add_child(disaster_instance)
 				await get_tree().create_timer(0.3).timeout
 				target_tile.trigger_disaster(12, disaster_instance)
-	elif roll <= 60:
-		print("Tornado dissipates at ", plot_index)
-	else:
-		print("Tornado continues raging at ", plot_index)
+	elif roll <= 60: # 25% chance to goney
+		# dissipate
+		pass
+	else: #hahahaha again!!!!
 		await get_tree().create_timer(0.8).timeout
 		var disaster_instance = disaster_scene.instantiate()
 		self.add_child(disaster_instance)
